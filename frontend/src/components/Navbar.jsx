bd5e527bedc6e2ed0dbc9a02a1fb4fa88d724b8d
Your Name <you@example.com>
2026-01-19 20:54:40 +0530

    First Commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         navigate('/login');
     };
     return (
-        <div className='flex items-center justify-between py-5 font-medium '>
+        <div className='flex items-center justify-between py-0 font-medium '>
             <Link to={'/'}><img src={assets.logo} className='w-36' alt='' /></Link>
 
             <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
