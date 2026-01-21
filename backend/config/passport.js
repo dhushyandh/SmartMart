9f788775a015f88600de4ae7652c8fd511636452
Your Name <you@example.com>
2026-01-21 21:46:36 +0530

    Initial Commit

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -7,9 +7,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.NODE_ENV === 'production'
-                ? "https://shopxy-backend.vercel.app/auth/google/callback"
-                : "http://localhost:4000/auth/google/callback",
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
