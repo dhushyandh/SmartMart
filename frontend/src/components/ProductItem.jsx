c85bf7151c0c7ebb0ff73b5c10d0a9800ddd877a
Your Name <you@example.com>
2026-01-22 20:34:22 +0530

    First Commit

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -3,22 +3,24 @@ import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom';
 
 const ProductItem = ({ id, image, name, price }) => {
-    const { currency } = useContext(ShopContext);
+  const { currency } = useContext(ShopContext);
 
-    return (
-        <Link to={`/product/${id}`} className="text-gray-700">
-            <div className="overflow-hidden">
-                <img
-                    src={image?.[0]?.url}
-                    className="hover:scale-110 transition"
-                    alt={name}
-                />
-            </div>
-            <p className="pt-3 text-sm">{name}</p>
-            <p className="text-sm font-medium">{currency} {price}</p>
-        </Link>
-    );
+  return (
+    <Link to={`/product/${id}`} className="text-gray-700 block">
+      <div className="w-full h-70 bg-gray-50 flex items-center justify-center overflow-hidden">
+        <img
+          src={image?.[0]?.url}
+          alt={name}
+          className="w-full h-full object-contain transition-transform duration-300 hover:scale-105"
+        />
+      </div>
+
+      <p className="pt-3 text-sm min-h-10">{name}</p>
+      <p className="text-sm font-medium">
+        {currency} {price}
+      </p>
+    </Link>
+  );
 };
 
-
-export default ProductItem
+export default ProductItem;
